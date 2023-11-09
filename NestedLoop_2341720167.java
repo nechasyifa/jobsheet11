3e08c1d44bc94c7b61cf535fd30f787dea519e2a
Your Name <you@example.com>
2023-11-09 23:18:46 +0700

    modifikasi pertanyaan2

--- NestedLoop_2341720167.java
+++ NestedLoop_2341720167.java
@@ -16,8 +16,8 @@ public class NestedLoop_2341720167 {
 
         for (int i = 0; i < temps.length; i++) {
             System.out.print("Kota ke-" + (i + 1) + ": ");
-            for (int j = 0; j < temps[0].length; j++) {
-                System.out.print(temps[i][j] + " ");
+            for (double temp : temps[i]) {
+                System.out.print(temp + " ");
             }
             System.out.println();
         }
