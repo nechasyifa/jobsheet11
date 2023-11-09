73568d9c5fd3220a8a63f174a629e713acbaeb20
Your Name <you@example.com>
2023-11-09 23:25:00 +0700

    modifikasi pertanyaan3

--- NestedLoop_2341720167.java
+++ NestedLoop_2341720167.java
@@ -4,13 +4,17 @@ public class NestedLoop_2341720167 {
     public static void main(String[] args) {
         Scanner input20 = new Scanner(System.in);
         double temps[][] = new double[5][7];
+        double rata2[] = new double[5];
 
         for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" + i);
+            double jml = 0.0; 
             for (int j = 0; j < temps[0].length; j++) {
                 System.out.print("Hari ke-" + (j + 1) + ": ");
                 temps[i][j] = input20.nextDouble();
+                jml += temps[i][j];
             }
+            rata2[i] = jml / temps[0].length;
             System.out.println();
         }
 
@@ -19,7 +23,7 @@ public class NestedLoop_2341720167 {
             for (double temp : temps[i]) {
                 System.out.print(temp + " ");
             }
-            System.out.println();
+            System.out.println("Rata-rata: " + rata2[i]);
         }
     }
 }
