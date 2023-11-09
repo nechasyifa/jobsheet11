4c80f55a7785e93ebb7e3e78d4c6e50e718e696a
Your Name <you@example.com>
2023-11-09 21:54:50 +0700

    jobsheet11

--- NestedLoop_2341720167.java
+++ NestedLoop_2341720167.java
@@ -3,13 +3,13 @@ import java.util.Scanner;
 public class NestedLoop_2341720167 {
     public static void main(String[] args) {
         Scanner input20 = new Scanner(System.in);
-        int temps[][] = new int[5][7];
+        double temps[][] = new double[5][7];
 
         for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" + i);
             for (int j = 0; j < temps[0].length; j++) {
                 System.out.print("Hari ke-" + (j + 1) + ": ");
-                temps[i][j] = input20.nextInt();
+                temps[i][j] = input20.nextDouble();
             }
             System.out.println();
         }
@@ -18,8 +18,8 @@ public class NestedLoop_2341720167 {
             System.out.print("Kota ke-" + (i + 1) + ": ");
             for (int j = 0; j < temps[0].length; j++) {
                 System.out.print(temps[i][j] + " ");
+            }
+            System.out.println();
         }
-        System.out.println();
     }
-}
 }
--- Star20.java
+++ Star20.java
@@ -7,7 +7,7 @@ public class Star20 {
         System.out.print("Masukkan nilai N = ");
         int N = input20.nextInt();
 
-        for(int i = 1; i <= N; i--) {
+        for(int i = 1; i <= N; i++) {
             System.out.print("*");
         }
     }
